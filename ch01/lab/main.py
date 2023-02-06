f96dbea8d1bf59407f8eb6fd01e2f97100bc3d2e
Kameron8 <kcummin4@binghamton.edu>
2023-02-06 12:02:41 -0500

    Monday Feb 6

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -3,18 +3,18 @@ import random
 #Part A
 weeks = 16
 print(weeks, type(weeks))
-classes = 5
+classes = 10
 print(classes, type(classes))
 tuition = 6000
 print(tuition, type(tuition))
 cost_per_week = ((tuition / classes) / weeks)
-print(cost_per_week, type(cost_per_week)) # Do I need this?
+print(cost_per_week, type(cost_per_week))
 print("Cost per week:", cost_per_week)
 classes_per_week = 3
-print(classes_per_week, type (classes_per_week)) #Do I need this?
+print(classes_per_week, type (classes_per_week))
 cost_per_class = cost_per_week / classes_per_week
-print(cost_per_class, type(cost_per_class)) #Do I need this?
-print("Nice message of my choice;", cost_per_class)
+print(cost_per_class, type(cost_per_class)) 
+print("How much you are paying per class;", cost_per_class)
 
 #Part B
 random_list = ["Dog", "Cat", "Fox", "Elephant", "Snake"]
