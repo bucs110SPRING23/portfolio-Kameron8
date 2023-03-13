356fa3ef03005edcb7945e3347b8745439bc020f
Kameron8 <kcummin4@binghamton.edu>
2023-03-13 17:29:24 -0400

    Mar 13 529pm

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,20 +1,21 @@
 def prompt():
      print("Please Enter A Number: ")
+     a = int(input(": "))
+     return a 
+
      
-def star_pyramid():
+def star_pyramid(a):
     for i in range(1,a+1):
             print(i*"* ", end= " ")
             print("\n")
 
-def rstar_pyramid():
-    for i in reversed(range(1,g+1)):
+def rstar_pyramid(a):
+    for i in reversed(range(1,a+1)):
          print(i*"* ", end= " ")
          print("\n")
 
+def main():
+     star_pyramid(prompt())
+     rstar_pyramid(prompt())
 
-prompt()
-a = int(input(": "))
-star_pyramid()
-prompt()
-g = int(input(": "))
-rstar_pyramid()
+main()
