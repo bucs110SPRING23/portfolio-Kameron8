d8d945c366f13cf8cc34741371d2b114fc223c31
Kameron8 <kcummin4@binghamton.edu>
2023-03-13 17:17:00 -0400

    Mar 13 5pm

---
+++ ch05/exercises/scope.py
@@ -0,0 +1,26 @@
+def multiply(a,b):
+    count = 1
+    c = 0
+    while b >= count:
+        c = c + a
+        count += 1
+    return c
+
+def exponent(a,b): 
+    count = 1
+    c = 1
+    while b >= count:
+        c = c * a
+        count += 1
+    return c 
+
+def square(a):
+    return exponent(a,2)
+
+
+
+print(multiply(5,10))
+print(exponent(3,3))
+print(square(4))
+
+
