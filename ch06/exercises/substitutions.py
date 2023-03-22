32f9635caa5f012409939ccec3a6c17b8dba0ef9
Kameron8 <kcummin4@binghamton.edu>
2023-03-22 18:03:06 -0400

    Mar 22 603pm

--- ch06/exercises/substitutions.py
+++ ch06/exercises/substitutions.py
@@ -1,7 +1,14 @@
 import json
 
-file_pointer = open("news.txt", "r")
-file_pointer2 = open("subs.json", "r")
-data = json.load(file_pointer2)
-for ch in file_pointer:
-    x = 2
+def main():
+    news = open("assets/news.txt", "r").read().lower()
+    subs_pointer = open("assets/subs.json", "r")
+    subs = json.load(subs_pointer) #Now need to use one list to modify another list, so which list should we loop through? 
+    for k, v in subs.items(): #Usually easier to loop through the dictionary list 
+        news = news.replace(k,v)
+
+    fptr = open("assets/betternews.txt", "w") #Creates new file 
+    fptr.write(news)
+    fptr.close()
+
+main()
