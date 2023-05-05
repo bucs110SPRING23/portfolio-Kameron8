83939bdda8a789171428d938203909cb9c307d21
Kameron8 <kcummin4@binghamton.edu>
2023-05-04 22:16:52 -0400

    May 4th 10:16pm

--- ch10/final/controller.py
+++ ch10/final/controller.py
@@ -13,7 +13,7 @@ class Controller:
         """
         for _ in range(5):
             print('\n')
-        print("Welcome to your weather report. Please enter your longitude and latitude to find out the temperature for today, the sunset time, and to recieve a fortune!")
+        print("Welcome to your weather report. Please enter your longitude and latitude to find out the temperature, the sunset time, and to recieve some advice for the day!")
         advice = AdviceSlipAPI()
         advice_slip = AdviceSlipAPI.get(advice)
         lat = str(input("Latitude: "))
@@ -24,6 +24,6 @@ class Controller:
         maxtemperature =  TemperatureAPI.getmax(tempmax)
         tempmin = TemperatureAPI(lat,lng)
         mintemperature =  TemperatureAPI.getmin(tempmin)
-        print(f"The sun will be setting at {sunset_time} UTC tonight. The high and low for today is {maxtemperature} and {mintemperature} degrees Fahrenheit. Finally, your advice for the day is '{advice_slip}'")
+        print(f"The sun will be setting at {sunset_time} UTC tonight. The high and low for today is {maxtemperature} and {mintemperature} degrees Fahrenheit. Finally, your advice for the day is '{advice_slip}' Enjoy the rest of your day!")
 
 
