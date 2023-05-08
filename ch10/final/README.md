b3c8b19511c06df0bfdb09d82dee296ab243d661
Kameron8 <kcummin4@binghamton.edu>
2023-05-08 17:16:27 -0400

    May 8th 516pm

--- ch10/final/README.md
+++ ch10/final/README.md
@@ -1,6 +1,6 @@
 # CS110 Final Exam
 
-## SHORT DESCRIPTION *My program asks for the user's latitude and longitude information and then uses that to create a personalized weather report which displays the highs and lows for the day, the time the sun will set and some advice to concliude the report.*
+## SHORT DESCRIPTION *My program asks for the user's latitude and longitude information and then uses that to create a personalized weather report which displays the highs and lows for the day, the time the sun will set and some advice to conclude the report.*
 
 ## KNOWN BUGS AND INCOMPLETE PARTS *Nope*
 
--- ch10/final/main.py
+++ ch10/final/main.py
@@ -1,11 +1,22 @@
 from adviceslip import AdviceSlipAPI
 from sunset import SunsetAPI
 from temperature import TemperatureAPI
-from controller import Controller
 import requests
 
 def main():
-    controller = Controller()
-    controller.mainloop()
+    for _ in range(5):
+        print('\n')
+    print("Welcome to your weather report. Please enter your longitude and latitude to find out the temperature, the sunset time, and to recieve some advice for the day!")
+    advice = AdviceSlipAPI()
+    advice_slip = AdviceSlipAPI.get(advice)
+    lat = str(input("Latitude: "))
+    lng = str(input("Longitude: "))
+    sunset = SunsetAPI(lat, lng)
+    sunset_time = SunsetAPI.get(sunset)
+    tempmax = TemperatureAPI(lat,lng)
+    maxtemperature =  TemperatureAPI.getmax(tempmax)
+    tempmin = TemperatureAPI(lat,lng)
+    mintemperature =  TemperatureAPI.getmin(tempmin)
+    print(f"The sun will be setting at {sunset_time} UTC tonight. The high and low for today is {maxtemperature} and {mintemperature} degrees Fahrenheit. Finally, your advice for the day is '{advice_slip}' Enjoy the rest of your day!")
 
 main()
